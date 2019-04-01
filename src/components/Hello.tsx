e84eed1acb13cb3370a740706a284e9aee188e5e
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-04-01 13:29:14 +0200

    centered text

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,4 +2,16 @@ import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) => <h1 style={centeredContent}>
+    Hello from {props.compiler} and {props.framework}!</h1>;
+
+const centeredContent: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: '0'
+}
