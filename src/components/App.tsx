ffb100707828a07cc0bbfffe706b3b0bd36094fb
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-04-01 15:40:01 +0200

    test imports ok!

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+//import * as React from "react";
+import React from "react";
+import Layout from "./Layout";
+
+//interface MyApp { compiler: string; framework: string; }
+
+export default function App() {
+    return <Layout/>;
+}
+
+/*const centeredContent: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: '0'
+}*/
---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import Header from "header";
+
+export default function Layout() {
+    return (
+        //<div style={contentLayout}>
+        <h1>test</h1>
+            
+        //</div>
+    );
+}
+
+/*const contentLayout: CSSProperties = {
+    width: '100%',
+    display: 'flex'
+}*/
